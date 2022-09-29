9c40a8ca97ca19cdff3a90de232421a82b73b7db
aleksa-blinova mail.ru <aleksa-blinova@mail.ru>
2022-09-29 19:09:28 +0300

    creacted app, downloaded fonts, started to work

---
+++ ox-st-test-task-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CarLeasingCalc from "./CarLeasingCalc/CarLeasingCalc";
+
+function App() {
+  return (
+    <div className="App">
+      <CarLeasingCalc />
+    </div>
+  );
+}
+
+export default App;
---
+++ ox-st-test-task-react/src/CarLeasingCalc/CarLeasingCalc.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./CarLeasingCalc.css";
+
+const CarLeasingCalc = () => {
+  return (
+    <>
+      <div className="header">
+        <h1 className="header-h1">Рассчитайте стоимость автомобиля в лизинг</h1>
+      </div>
+    </>
+  );
+};
+
+export default CarLeasingCalc;
