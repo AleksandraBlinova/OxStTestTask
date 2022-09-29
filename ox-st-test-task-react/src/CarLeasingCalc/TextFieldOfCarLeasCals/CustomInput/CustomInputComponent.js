cd72d901449551c88772239d42aba405b85d0be5
aleksa-blinova mail.ru <aleksa-blinova@mail.ru>
2022-09-30 00:24:38 +0300

    hardcoded slider

--- ox-st-test-task-react/src/CarLeasingCalc/TextFieldOfCarLeasCals/CustomInput/CustomInputComponent.js
+++ ox-st-test-task-react/src/CarLeasingCalc/TextFieldOfCarLeasCals/CustomInput/CustomInputComponent.js
@@ -8,6 +8,8 @@ export const CustomInput = styled(InputBase)(({ theme }) => ({
     backgroundColor: "#F3F3F4",
     border: "none",
     fontSize: 30,
+    marginBottom: 0,
+
     fontFamily: "Nekst-Black",
     color: "#575757",
     padding: "10px 12px",
--- ox-st-test-task-react/src/CarLeasingCalc/TextFieldOfCarLeasCals/TextFieldOfCarLeasCals.js
+++ ox-st-test-task-react/src/CarLeasingCalc/TextFieldOfCarLeasCals/TextFieldOfCarLeasCals.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./TextFieldOfCarLeasCals.css";
 import { CustomInput } from "./CustomInput/CustomInputComponent";
+import { CustomSliderComponent } from "./CustomSlider/CustomSliderComponent";
 
 const TextFieldOfCarLeasCals = (props) => {
-  console.log(props.calcParametrs);
   return (
     <div className="text-fields">
       {props.calcParametrs.map((calcParametr) => (
         <div className="text-field" key={calcParametr.id}>
           <p className="text-field-p-text">{calcParametr.name}</p>
           <CustomInput value={calcParametr.value} />
+          <CustomSliderComponent />
         </div>
       ))}
     </div>
