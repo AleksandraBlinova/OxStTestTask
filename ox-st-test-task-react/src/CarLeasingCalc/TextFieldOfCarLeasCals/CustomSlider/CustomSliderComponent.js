dacd61eb2f5372eab67b83111f8f455dceeb6845
aleksa-blinova mail.ru <aleksa-blinova@mail.ru>
2022-09-30 00:36:23 +0300

    fixed some bugs

--- ox-st-test-task-react/src/CarLeasingCalc/TextFieldOfCarLeasCals/CustomSlider/CustomSliderComponent.js
+++ ox-st-test-task-react/src/CarLeasingCalc/TextFieldOfCarLeasCals/CustomSlider/CustomSliderComponent.js
@@ -1,41 +1,29 @@
 import React from "react";
 import Slider from "@mui/material/Slider";
 import { styled } from "@mui/material/styles";
+import "./CustomSliderComponent.css";
 
 export const CustomSliderComponent = styled(Slider)({
   color: "#FF9514",
-  width: 380,
+  width: 360,
   height: 2,
   marginTop: -20,
-  paddingTop: 0,
   marginLeft: 6,
   "& .MuiSlider-track": {
     border: "none",
   },
+  "& .MuiSlider-rail": {
+    opacity: 1,
+    color: "#E1E1E1",
+  },
   "& .MuiSlider-thumb": {
     height: 20,
     width: 20,
-    backgroundColor: "#FF9514",
     "&:focus, &:hover, &.Mui-active, &.Mui-focusVisible": {
       boxShadow: "inherit",
     },
     "&:before": {
       display: "none",
-    },
-  },
-  "& .MuiSlider-valueLabel": {
-    fontSize: 12,
-    background: "unset",
-    padding: 0,
-    borderRadius: "50% 50% 50% 0",
-    transformOrigin: "bottom left",
-    transform: "translate(50%, -100%) rotate(-45deg) scale(0)",
-    "&:before": { display: "none" },
-    "&.MuiSlider-valueLabelOpen": {
-      transform: "translate(50%, -100%) rotate(-45deg) scale(1)",
-    },
-    "& > *": {
-      transform: "rotate(45deg)",
     },
   },
 });
