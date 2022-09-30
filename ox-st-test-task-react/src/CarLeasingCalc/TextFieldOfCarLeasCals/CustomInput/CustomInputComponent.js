773315c3f7751b454968d66cb81749d014b41de6
aleksa-blinova mail.ru <aleksa-blinova@mail.ru>
2022-09-30 11:52:34 +0300

    fixed bugs in styling input components

--- ox-st-test-task-react/src/CarLeasingCalc/TextFieldOfCarLeasCals/CustomInput/CustomInputComponent.js
+++ ox-st-test-task-react/src/CarLeasingCalc/TextFieldOfCarLeasCals/CustomInput/CustomInputComponent.js
@@ -8,19 +8,10 @@ export const CustomInput = styled(InputBase)(({ theme }) => ({
     backgroundColor: "#F3F3F4",
     border: "none",
     fontSize: 30,
+    width: 403,
     marginBottom: 0,
-
     fontFamily: "Nekst-Black",
     color: "#575757",
-    padding: "10px 12px",
-    transition: theme.transitions.create([
-      "border-color",
-      "background-color",
-      "box-shadow",
-    ]),
-    // Use the system font instead of the default Roboto font.
-    "&:focus": {
-      borderColor: theme.palette.primary.main,
-    },
+    paddingLeft: "24px",
   },
 }));
